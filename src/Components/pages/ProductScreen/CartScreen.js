2c79434629031259ca46a47b40a832c412cb7fdf
Diana-nyamai <dnyamai.dn@gmail.com>
2020-11-11 06:12:55 +0300

    update

--- src/Components/pages/ProductScreen/CartScreen.js
+++ src/Components/pages/ProductScreen/CartScreen.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 function CartScreen(props) {
     const productId = props.match.params.id;
+    const qty = props.location.search? Number(props.location.search.split("=") [1]):1;
     return <div>
             <h1>cart screen</h1>
         </div>
