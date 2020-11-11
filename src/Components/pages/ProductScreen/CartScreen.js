ecc67a4c84c7505208d0d1e853372055f4b08f72
Diana-nyamai <dnyamai.dn@gmail.com>
2020-11-11 06:17:43 +0300

    update

--- src/Components/pages/ProductScreen/CartScreen.js
+++ src/Components/pages/ProductScreen/CartScreen.js
@@ -1,8 +1,14 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 
 function CartScreen(props) {
     const productId = props.match.params.id;
     const qty = props.location.search? Number(props.location.search.split("=") [1]):1;
+
+    useEffect(() => {
+        if(productId){
+            dispatch(addToCart(productId, qty));
+        }
+    }, [])
     return <div>
             <h1>cart screen</h1>
         </div>
