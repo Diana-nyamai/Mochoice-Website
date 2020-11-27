09a9ac898d715434c41c9ebf4156c912b623d84b
Diana-nyamai <dnyamai.dn@gmail.com>
2020-11-27 21:55:29 +0300

    cart screen update

--- src/Components/pages/ProductScreen/CartScreen.js
+++ src/Components/pages/ProductScreen/CartScreen.js
@@ -3,6 +3,7 @@ import { addToCart } from '../../../actions/CartActions';
 import {useDispatch} from 'react-redux';
 
 function CartScreen(props) {
+    
     const productId = props.match.params.id;
     const qty = props.location.search? Number(props.location.search.split("=")[1]):1;
     const dispatch = useDispatch();
