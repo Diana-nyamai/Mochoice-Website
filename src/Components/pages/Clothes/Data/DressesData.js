d13b77da8b31ae5b0fb7de07c807003eb2a2a1f8
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-14 17:12:47 +0300

    images

---
+++ src/Components/pages/Clothes/Data/DressesData.js
@@ -0,0 +1,136 @@
+export default  {
+    products:[
+        {
+        _id:'1',
+        name:'wedges',
+        category:'bags',
+        images:'/images/clothes/dresses/d1.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'2',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/dresses/d2.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'3',
+        name:'flowery wedge',
+        category:'bags',
+        images:'/images/clothes/dresses/d3.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    { 
+        _id:'4',
+        name:'animal print',
+        category:'bags',
+        images:'/images/clothes/dresses/d4.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'5',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/dresses/d5.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'6',
+        name:'doll shoes',
+        category:'bags',
+        images:'/images/clothes/dresses/d6.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'7',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/dresses/d7.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'8',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/dresses/d8.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'9',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/dresses/d9.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'10',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/dresses/d10.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'11',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/dresses/d11.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'12',
+        name:'wedge',
+        category:'bags',
+        images:'/images/clothes/dresses/d12.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+]
+}
