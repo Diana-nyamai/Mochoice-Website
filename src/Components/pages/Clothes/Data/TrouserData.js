127f32d06222bdca277673c9fb11bfbfe3f8aed5
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-14 17:36:49 +0300

    trouser data

---
+++ src/Components/pages/Clothes/Data/TrouserData.js
@@ -0,0 +1,136 @@
+export default  {
+    products:[
+        {
+        _id:'1',
+        name:'wedges',
+        category:'bags',
+        images:'/images/clothes/trouser/t1.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'2',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/trouser/t2.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'3',
+        name:'flowery wedge',
+        category:'bags',
+        images:'/images/clothes/trouser/t3.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    { 
+        _id:'4',
+        name:'animal print',
+        category:'bags',
+        images:'/images/clothes/trouser/t4.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'5',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/trouser/t5.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'6',
+        name:'doll shoes',
+        category:'bags',
+        images:'/images/clothes/trouser/t6.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'7',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/trouser/t7.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'8',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/trouser/t8.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'9',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/trouser/t9.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'10',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/trouser/t10.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'11',
+        name:'sneakers',
+        category:'bags',
+        images:'/images/clothes/trouser/t11.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+    {
+        _id:'12',
+        name:'wedge',
+        category:'bags',
+        images:'/images/clothes/trouser/t12.jpg',
+        price:6000,
+        brand:'LV',
+        rating:4.5,
+        numReviews:10
+        
+    },
+]
+}
