d590fcfc6fe8e1e95dbfd0af470cf3990b956e4f
Diana-nyamai <dnyamai.dn@gmail.com>
2020-12-14 16:36:18 +0300

    update

--- src/Components/pages/ShopPage/Watches.js
+++ src/Components/pages/ShopPage/Watches.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import WatchData from './Data/WatchData';
+import {Link} from 'react-router-dom';
 
 export class Watches extends Component {
     render() {
         return (
-            <div>
-                <h1>watches Page</h1>
-            </div>
+            <div className="content">
+            <ul className="products">
+                {
+                WatchData.products.map(product => 
+                <li key={product._id}>
+                    <div className="product">
+                    <Link to={'/Product/' + product._id}>
+                        <img className="product-image" src ={product.images} alt="bag"/>
+                        </Link>
+                        <div className="product-name"><Link to={'/Product/' + product._id}>{product.name}</Link></div>
+                        <div className="product-brand">{product.brand}</div>
+                        <div className="product-price">Ksh.{product.price}</div>
+                        <div className="product-rating">4.5 stars ({product.numReviews})</div>
+                        </div>
+    
+                </li>    
+                )
+                    }
+                
+            </ul>
+        </div>
         )
     }
 }
